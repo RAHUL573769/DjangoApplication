53a953e08e98f5f272fe6200abdd8bcc052368cd
Rahul Rudra <rahulrudra665@gmail.com>
2023-08-28 10:39:42 +0600

    template commit

--- thirdproject/urls.py
+++ thirdproject/urls.py
@@ -17,5 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.urls import include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", views.home)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("firstapp/", include("firstapp.urls")),
+]
